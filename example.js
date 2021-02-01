7e8743ce2dd97883c366d862f3e0d6d0788faa78
Adria Gonzalez <adriagc8@pop-os.localdomain>
2021-02-01 17:39:48 +0100

    basic conf

--- example.js
+++ example.js
@@ -1,9 +1,21 @@
-const bigbluebot = require('./index.js');
+// const bigbluebot = require('./index.js');
+
+// const actions = async page => {
+//   await bigbluebot.audio.modal.microphone(page);
+//   await bigbluebot.video.join(page);
+//   await bigbluebot.chat.send(page);
+// };
+
+// bigbluebot.run(actions);
+const bigbluebot = require('bigbluebot');
 
 const actions = async page => {
-  await bigbluebot.audio.modal.microphone(page);
-  await bigbluebot.video.join(page);
+  await bigbluebot.audio.modal.listen(page);
   await bigbluebot.chat.send(page);
 };
 
-bigbluebot.run(actions);
+const options = {
+  moderator: false
+};
+
+bigbluebot.run(actions, options);
